0c03c8176a291eb21109c579bd0a1099d0064c6b
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-02-07 14:46:51 -0500

    added get before ('status)'

--- test/test-vioth-integration.js
+++ test/test-vioth-integration.js
@@ -7,7 +7,7 @@ describe('GET endpoint', function() {
   it('should respond with a 200 status code', function() {
     let res
     return chai.request(app)
-      .('/status')
+      .get('/status')
       .then(function(_res) {
         res = _res
         expect(res).to.have.status(200)
