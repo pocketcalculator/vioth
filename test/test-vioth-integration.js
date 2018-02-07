b18e7d4ada1df9436ccb5f05fa0f29a9f5489875
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-02-07 14:49:43 -0500

    added app const from require server.js

--- test/test-vioth-integration.js
+++ test/test-vioth-integration.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-
+const chaiHttp = require('chai-http')
+const app = require('../server')
 const expect = chai.expect
 
 describe('GET endpoint', function() {
