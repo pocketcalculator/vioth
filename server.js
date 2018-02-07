0b4dcd4bf4268b5772135d9cd6eb61b0a6be7d50
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-02-07 14:37:56 -0500

    added get /status test

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
 app.use(express.static('public'))
+
+app.get('/status', (req, res) => {
+  res.status('200').json()
+}
+
 app.listen(process.env.PORT || 8080)
