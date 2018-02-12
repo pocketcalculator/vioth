4cef7219d8e5f8232c6c846e131c3c4d774b22da
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-02-12 13:09:07 -0500

    adding test to validate JSON data in GET

--- test/test-vioth-integration.js
+++ test/test-vioth-integration.js
@@ -16,15 +16,25 @@ describe('GET endpoint', function() {
       })
   })
 })
-
+// 1. make a request to `/status`
+// 2. inspect response object and ensure code is correct and has
+//    matching keys.
 describe('GET endpoint', function() {
-  it('should respond with a 200 status code', function() {
+  it('should list items on GET', function() {
     let res
     return chai.request(app)
       .get('/status')
       .then(function(_res) {
         res = _res
         expect(res).to.have.status(200)
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array')
+        expect(res.body.length).to.be.at.least(1)
+// each item should be an object with correct key/value pairs
+        const expectedKeys = ['id', 'name', 'installedDate', 'safeTempThreshold', 'isHuman'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
       })
   })
 })
