98a282b0c042f3af7f4d6f539056913a2d7d9e28
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-02-12 11:21:48 -0500

    created model

---
+++ model.js
@@ -0,0 +1,39 @@
+const uuid = require('uuid')
+
+// this module provides volatile storage for our systemComponent model.
+
+function StorageException(message) {
+  this.message = message
+  this.name = "StorageException"
+}
+
+const systemComponent = {
+  create: function(deviceId, name, installedDate, healthyTempThreshold, isHuman) {
+    console.log(`creating a new system component`)
+    const item = {
+      id: uuid.v4()
+      name: name,
+      installedDate: Date.now(),
+      healthyTempThreshold: healthyTempThreshold
+    }
+    this.items[item.id] = item
+    return item
+  }
+  get: function() {
+    console.log(`Retrieving system component list...`)
+    return Object.keys(this.items).map(key => this.items[key])
+  }
+  delete: function(itemId) {
+    console.log(`Deleting system component \`${itemId}\``)
+    delete this.items[itemId]
+  }
+  update: function(updatedItem) {
+    console.log(`Updating system component \`${itemId}\``)
+    const {id} = updatedItem
+    if (!(id in this.items)) {
+      throw StorageException(`\`${id}\` doesn't exist`)
+    }
+    this.items[updatedItem.id] = updatedItem
+    return updatedItem
+  }
+}
