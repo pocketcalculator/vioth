4431b76c072a8aec2fd175c63c71f20c904d746b
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-03-10 21:41:21 -0500

    missing body-parser def

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 mongoose.Promise = global.Promise
 
 const { DATABASE_URL, PORT } = require('./config')
