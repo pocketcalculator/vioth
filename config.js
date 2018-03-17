1b28a5bbf9f21179a8dd1b90e30d6b81c7d9a8df
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-03-17 16:43:38 -0400

    added JWT info

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://vioth-dbuser:8Rb4ZVBDbnq5@ds261838.mlab.com:61838/pocketcalculator-vioth'
 exports.PORT = process.env.PORT || 8080
+exports.JWT_SECRET = process.env.JWT_SECRET || 'stereo2go'
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
