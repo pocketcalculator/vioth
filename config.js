9ae7d78320316f6dbeabe61b85c4ef5f5c24322c
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-05-03 09:51:53 -0400

    adding test mongo db

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-vioth'
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://vioth-dbuser:8Rb4ZVBDbnq5@ds113700.mlab.com:13700/pocketcalculator-vioth-test'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/vioth'
+/*
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://vioth-dbuser:8Rb4ZVBDbnq5@ds261838.mlab.com:61838/pocketcalculator-vioth'
+*/
 exports.PORT = process.env.PORT || 8080
 exports.JWT_SECRET = process.env.JWT_SECRET || 'stereo2go'
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
