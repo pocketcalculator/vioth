404e42c14f22cdf6c6582490000a0baac9c04f11
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-05-03 09:21:15 -0400

    adding test mongo db

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-vioth'
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://vioth-dbuser:8Rb4ZVBDbnq5@ds261838.mlab.com:61838/pocketcalculator-vioth'
 exports.PORT = process.env.PORT || 8080
 exports.JWT_SECRET = process.env.JWT_SECRET || 'stereo2go'
--- test/test-vioth-integration.js
+++ test/test-vioth-integration.js
@@ -12,7 +12,7 @@ const {
   closeServer
 } = require('../server')
 const {
-  DATABASE_URL
+  TEST_DATABASE_URL
 } = require('../config')
 
 chai.use(chaiHttp)
